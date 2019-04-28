ba00f22b22ae52d838b0745a579d61bcb9a96e9c
Victor <adsvictor@outlook.com>
2019-04-28 11:36:28 -0300

    Curso Go - aula 01

---
+++ YouTube/EuProgramador/Curso Go/aula01/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	number1, number2 := 10, 2
+
+	fmt.Print(number1 + number2)
+	fmt.Print(number1 - number2)
+	fmt.Print(number1 * number2)
+	fmt.Print(number1 / number2)
+}
