6a08197bd90e665cfcb08bc7251320c8a3f6d805
Victor <adsvictor@outlook.com>
2019-06-04 13:36:27 -0300

    Refactoring Arrow functions

--- Udemy/Desenvolvedor Multiplataforma Android ou IOS com React e Redux/app1/App.js
+++ Udemy/Desenvolvedor Multiplataforma Android ou IOS com React e Redux/app1/App.js
@@ -2,12 +2,12 @@ var React = require('react');
 var Text = require('react-native').Text;
 var AppRegistry = require('react-native').AppRegistry;
 
-const App = function(){
+const App = () => {
    return (
        <Text>Meu primeiro APP</Text>
    ); 
 };
 
-AppRegistry.registerComponent('app1', function(){return App});
+AppRegistry.registerComponent('app1', () => App);
 
 export default App;
