ed9ab89bac17dbc36692ccf41479894e027218c6
Victor <adsvictor@outlook.com>
2019-06-04 14:01:25 -0300

    Gerando e exibindo números aleatórios

--- Udemy/Desenvolvedor Multiplataforma Android ou IOS com React e Redux/app1/App.js
+++ Udemy/Desenvolvedor Multiplataforma Android ou IOS com React e Redux/app1/App.js
@@ -1,10 +1,24 @@
 var React = require('react');
 var Text = require('react-native').Text;
+var View = require('react-native').View;
+var Button = require('react-native').Button;
 var AppRegistry = require('react-native').AppRegistry;
+
+const geraNumeroAleatorio = () => {
+		var numero_aleatorio = Math.random();
+		numero_aleatorio = Math.floor(numero_aleatorio * 10);
+		alert(numero_aleatorio);
+};
 
 const App = () => {
    return (
-       <Text>Meu primeiro APP</Text>
+       <View>
+        <Text>Gerador de números randômicos!!!</Text>
+        <Button 
+        	title="Gerar um número randômico"
+					onPress={geraNumeroAleatorio} 
+        />
+       </View>
    ); 
 };
 
