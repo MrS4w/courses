eeb038b6a117d871c104bddf6fc9ef5d3990d5a0
Victor <adsvictor@outlook.com>
2020-03-09 18:13:49 -0300

    Ident app1

--- Udemy/Desenvolvedor Multiplataforma Android ou IOS com React e Redux/app1/App.js
+++ Udemy/Desenvolvedor Multiplataforma Android ou IOS com React e Redux/app1/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import { Text, View, Button, AppRegistry } from 'react-native';
+import React from "react";
+import { Text, View, Button, AppRegistry } from "react-native";
 
 const geraNumeroAleatorio = () => {
-		var numero_aleatorio = Math.random();
-		numero_aleatorio = Math.floor(numero_aleatorio * 10);
-		alert(numero_aleatorio);
+  var numero_aleatorio = Math.random();
+  numero_aleatorio = Math.floor(numero_aleatorio * 10);
+  alert(numero_aleatorio);
 };
 
 const App = () => {
-   return (
-       <View>
-        <Text>Gerador de números randômicos!!!</Text>
-        <Button 
-        	title="Gerar um número randômico"
-					onPress={geraNumeroAleatorio} 
-        />
-       </View>
-   ); 
+  return (
+    <View>
+      <Text>Gerador de números randômicos!!!</Text>
+      <Button title="Gerar um número randômico" onPress={geraNumeroAleatorio} />
+    </View>
+  );
 };
 
-AppRegistry.registerComponent('app1', () => App);
+AppRegistry.registerComponent("app1", () => App);
 
 export default App;
